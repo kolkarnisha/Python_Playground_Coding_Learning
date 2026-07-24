5986e4e399ec415c5713755a4baa2c1a0d793be2
Kolkar Nisha <neeshakolkar@gmail.com>
2026-07-24 11:02:58 +0530

    Implement TV class with power on functionality

---
+++ anstraction.py
@@ -0,0 +1,13 @@
+class TV:
+  def turn_on(self):
+    sel.__power_supply()
+    self.__motherboard()
+    self.__display()
+ def __power_supply(self):
+   print("power on")
+ def __motherboard(self):
+   print("motherboard started")
+def __display(self):
+  print("display activated")
+tv=TV()
+tv.turn_on()
