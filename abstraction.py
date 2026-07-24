e08da9a2947dab15ae88b68cab455c680e52232f
Kolkar Nisha <neeshakolkar@gmail.com>
2026-07-24 11:24:36 +0530

    Implement e-commerce payment gateway
    Added an implementation of an e-commerce payment gateway with CreditCard, UPI, and PayPal classes.

--- abstraction.py
+++ abstraction.py
@@ -37,3 +37,27 @@ class car(vehicle):
           print("car strted")
 car=car()
 car.start()
+# implementation of ecommerce gateway
+
+from abc import ABC, abstractmethod
+class Payment(ABC):
+
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class CreditCard(Payment):
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using Credit Card")
+
+class UPI(Payment):
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using UPI")
+
+class PayPal(Payment):
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using PayPal")
+
+payments = [CreditCard(),UPI(),PayPal()]
+for payment in payments:
+    payment.pay(500)
